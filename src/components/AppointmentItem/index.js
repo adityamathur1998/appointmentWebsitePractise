ca454baa8dd318b1ca9e15b436cd0c0a3ebbf875
adityamathur <eraditya0024@gmail.com>
2023-10-24 07:45:15 +0000

    mycodinganswer

---
+++ src/components/AppointmentItem/index.js
@@ -0,0 +1,34 @@
+// Write your code here
+import './index.css'
+
+const AppointmentItem = props => {
+  const {appointmentDetails, toggleIsStarred} = props
+  const {id, title, date, isImportant} = appointmentDetails
+
+  const starImgUrl = isImportant
+    ? 'https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png'
+    : 'https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png'
+
+  const onClickStar = () => {
+    toggleIsStarred(id)
+  }
+
+  return (
+    <li className="appointment-item">
+      <div className="header-container d-flex justify-content-between align-items-center w-100">
+        <p className="title">{title}</p>
+        <button
+          type="button"
+          data-testid="star"
+          className="star-button"
+          onClick={onClickStar}
+        >
+          <img src={starImgUrl} className="star" alt="star" />
+        </button>
+      </div>
+      <p className="date">Date: {date}</p>
+    </li>
+  )
+}
+
+export default AppointmentItem
---
+++ src/components/Appointments/index.js
@@ -0,0 +1,152 @@
+// Write your code here
+import {Component} from 'react'
+import {v4} from 'uuid'
+import {format} from 'date-fns'
+
+import AppointmentItem from '../AppointmentItem'
+
+import './index.css'
+
+class Appointments extends Component {
+  state = {
+    titleInput: '',
+    dateInput: '',
+    appointmentList: [],
+    isFilterActive: false,
+  }
+
+  toggleIsStarred = id => {
+    this.setState(prevState => ({
+      appointmentList: prevState.appointmentList.map(eachAppointment => {
+        if (id === eachAppointment.id) {
+          return {...eachAppointment, isStarred: !eachAppointment.isStarred}
+        }
+        return eachAppointment
+      }),
+    }))
+  }
+
+  onClickFilter = () => {
+    const {isFilterActive} = this.state
+    this.setState({
+      isFilterActive: !isFilterActive,
+    })
+  }
+
+  addAppointments = event => {
+    event.preventDefault()
+    const {titleInput, dateInput} = this.state
+
+    const formattedDate = dateInput
+      ? format(new Date(dateInput), 'dd MMMM yyyy, EEEE')
+      : ''
+
+    const newAppointment = {
+      id: v4(),
+      title: titleInput,
+      date: formattedDate,
+      isStarred: false,
+    }
+
+    this.setState(prevState => ({
+      appointmentList: [...prevState.appointmentList, newAppointment],
+      titleInput: '',
+      dateInput: '',
+    }))
+  }
+
+  onChangeTitleInput = event => {
+    this.setState({titleInput: event.target.value})
+  }
+
+  onChangeDateInput = event => {
+    this.setState({dateInput: event.target.value})
+  }
+
+  getFilteredAppointmentsList = () => {
+    const {appointmentList, isFilterActive} = this.state
+    if (isFilterActive) {
+      return appointmentList.filter(
+        eachAppointment => eachAppointment.isStarred === true,
+      )
+    }
+    return appointmentList
+  }
+
+  render() {
+    const {titleInput, dateInput, isFilterActive} = this.state
+    const filterClassName = isFilterActive ? 'filter-filled' : 'filter-empty'
+    const filteredAppointmentsList = this.getFilteredAppointmentsList()
+
+    return (
+      <div className="app-container d-flex justify-content-center align-items-center">
+        <div className="responsive-container d-flex justify-content-center align-items-center">
+          <div className="appointments-container d-flex flex-column justify-content-center align-items-center">
+            <div className="add-appointment-container d-flex justify-content-between">
+              <form
+                className="appointment-form d-flex flex-column w-100 order-1 order-md-0"
+                onSubmit={this.onAddAppointment}
+              >
+                <h1 className="add-appointment-text-heading">
+                  Add Appointment
+                </h1>
+                <label htmlFor="title" className="label">
+                  TITLE
+                </label>
+                <input
+                  type="text"
+                  value={titleInput}
+                  id="title"
+                  onChange={this.onChangeTitleInput}
+                  className="input"
+                  placeholder="Title"
+                  autoComplete="OFF"
+                />
+                <label htmlFor="date" className="label">
+                  DATE
+                </label>
+                <input
+                  type="date"
+                  value={dateInput}
+                  id="date"
+                  className="input"
+                  onChange={this.onChangeDateInput}
+                />
+                <button type="submit" className="add-button align-self-start">
+                  Add
+                </button>
+              </form>
+              <img
+                src="https://assets.ccbp.in/frontend/react-js/appointments-app/appointments-img.png"
+                alt="appointments"
+                className="appointments-image order-0 order-md-1"
+              />
+            </div>
+            <hr className="hr" />
+            <div className="header-with-filter-container d-flex justify-content-between align-items-center align-self-start">
+              <h1 className="appointments-heading">Appointments</h1>
+              <button
+                type="button"
+                className={`filter-style ${filterClassName}`}
+                onClick={this.onClickFilter}
+              >
+                Starred
+              </button>
+            </div>
+            <ul className="appointment-list d-flex flex-column flex-md-row flex-md-wrap w-100 p-0">
+              {filteredAppointmentsList.map(eachAppointment => (
+                <AppointmentItem
+                  key={eachAppointment.id}
+                  appointmentDetails={eachAppointment}
+                  toggleIsStarred={this.toggleIsStarred}
+                />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Appointments
